fb0ab168f74a22a5ca2d506d2eb16be79f1f3829
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2015-05-25 00:50:20 +0200

    made working the final part of Roland on http, curl -T /dev/zero http://localhost:8080/upload > /dev/null the final /dev/null is really important otherwise we don't see the statistics (!)

--- src/main/scala/TheDemo70_Http.scala
+++ src/main/scala/TheDemo70_Http.scala
@@ -20,23 +20,20 @@ object TheDemo70_Http extends App {
   implicit val sys=ActorSystem("TheDemo")
   implicit val mat=ActorFlowMaterializer()
   //implicit val timeout = Timeout(3.seconds)
-  def delay(x:ByteString)  =
-    after(1.second, sys.scheduler)(Future.successful(x))
 
 
-  val f = Flow[ByteString].
-    mapAsync(4)(delay)
+  val f = Flow[ByteString].mapAsync(4)(x => after(0.5 seconds, sys.scheduler)(Future.successful(x)))
 
   val route = pathPrefix("demo"){
-    getFromBrowseableDirectories("d:\\apps")
+    getFromBrowseableDirectories("/Users/pakkio")
   } ~
-  path("upload"){
-    extractRequest{
-      req =>
-        req.entity.dataBytes.via(f).to(Sink.ignore).run()
-        complete(StatusCodes.OK)
+    path("upload"){
+      extractRequest{
+        req =>
+          req.entity.dataBytes.via(f).to(Sink.ignore).run()
+          complete(StatusCodes.OK)
+      }
     }
-  }
 
   Http().bind("localhost",8080)
     .runForeach(conn => conn.flow.join(route).run()) // remember the inner run() !!!
